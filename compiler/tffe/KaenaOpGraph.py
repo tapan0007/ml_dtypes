60bdb15489d1497cd56472d9fcf65d400271f80b
Jindrich Zejda <zejdaj@amazon.com>
2017-10-18 16:23:43 +0000

    Separated tensorflow frontend into layers - op graph, translation and user.

---
+++ compiler/tffe/KaenaOpGraph.py
@@ -0,0 +1,63 @@
+# Kaena absration of neural network framework operations
+# Suitable for TF freeze graph input and general op reduction and fusing
+
+
+class Object:
+  def __init__(self, name, attrs):
+    self.__name = name
+    self.__attrs = attrs
+  def getName(self):
+    return(self.__name)
+  def getAttr(self, attrName):
+    return(self.__attrs[attrName])
+
+class Node(Object):
+  def __str__(self):
+    return("Node=" + self.getName())
+
+class Edge(Object):
+  def __init__(self, fromNode, toNode, attrs):
+    Object.__init__(self, "Edge", attrs)
+    self.__fromNode = fromNode
+    self.__toNode = toNode
+  def fromNode(self):
+    return(self.__fromNode)
+  def toNode(self):
+    return(self.__toNode)
+  def __str__(self):
+    return("Edge  From=" + self.fromNode().getName()
+           + "  To=" + self.toNode().getName())
+
+class Graph(Object):
+  def __init__(self, name, attrs = {}):
+    Object.__init__(self, name, attrs)
+    self.__name2node = {}
+    self.__edges = {}
+    
+  def addNode(self, name, attrs = {}):
+    self.__name2node[name] = Node(name, attrs)
+  
+  def addEdge(self, nameFrom, nameTo, attrs = {}):
+    fromNode = self.getNode(nameFrom)
+    toNode = self.getNode(nameTo)
+    if self.__edges.get(fromNode) == None:
+      self.__edges[fromNode] = {}
+    self.__edges[fromNode][toNode] = Edge(fromNode, toNode, attrs)
+
+  def getNode(self, name):
+    return(self.__name2node[name])
+  
+  def getEdge(self, nameFrom, NameTo):
+    return(self.__name2node[name])
+  
+  def getNodes(self):
+    return(self.__name2node.values())
+  
+  def getEdges(self):
+    edges = []
+    for fromNode in self.__edges:
+      edges += list(self.__edges[fromNode].values())
+    return(edges)
+
+
+
---
+++ compiler/tffe/TfFrontEnd.py
@@ -0,0 +1,130 @@
+# TensorFlow front end for Kaena Compiler
+
+# Nodes etc
+#   https://www.tensorflow.org/extend/tool_developers/
+
+# Parsing of pb file - sample pb
+#   https://storage.googleapis.com/download.tensorflow.org/models/inception_v3_2016_08_28_frozen.pb.tar.gz
+
+import tensorflow as tf
+import numpy as np   
+from tensorflow.python.platform import gfile
+from tensorflow.core.framework import graph_pb2
+from tensorflow.python.framework import tensor_util
+from graphviz import Digraph
+import re
+import KaenaOpGraph as kog
+
+class TfOp:
+  def __init__(self, name, op, tfNode):
+    self.name = name
+    self.op = op
+    self.tfNode = tfNode
+  def __str__(self):
+    return("Name=" + self.name + "  Op=" + self.op)
+  def getTensor(self):
+    attr = self.tfNode.attr
+    #print("  Attr=", attr)
+    tensor = attr['value'].tensor
+    return(tensor)
+  def getTensorNd(self):
+    tensor = self.getTensor()
+    #print("  Tensor=", tfTensor.tensor_shape, type(t))
+    shapeStr = ""
+    try:
+      nd = tensor_util.MakeNdarray(tensor)
+      #print("  Nd=", nd.shape)
+      shapeStr = " Shape " + "x".join(str(x) for x in nd.shape)
+      #print("  Nd1=", nd.shape, "ShapeStr=", shapeStr)
+    except :
+      #print("  Nd=Failed")
+      shapeStr = ""
+      nd = np.array([])
+    return(nd)
+
+class TfFe:
+  def __init__(self):
+    self.__gd = None
+    self.__kg = None
+  
+  def loadPb(self, pbFile):
+    self.__gd = graph_pb2.GraphDef()
+    with gfile.FastGFile(pbFile,'rb') as f:
+      self.__gd.ParseFromString(f.read())
+    
+    self.__kg = kog.Graph(pbFile)
+    
+    for tfNode in self.__gd.node:
+      tfop = TfOp(tfNode.name, tfNode.op, tfNode)
+      #print(tfop)
+      self.__kg.addNode(tfNode.name, {"tfop" : tfop})
+      
+      for ni in tfNode.input:
+        #print("  Input=", ni)
+        self.__kg.addEdge(ni, tfNode.name)
+    
+  def writeWeights(self, outPrefix):
+    numWeights = 0
+    for n in self.__kg.getNodes():
+      tfOp = n.getAttr("tfop")
+      #tfNode = tfOp.tfNode
+      nd = tfOp.getTensorNd()
+      if (nd.size > 0):
+        weightFile = outPrefix + tfOp.name.replace("/", "__")
+        #print("WeightFile=", weightFile,
+        #      "  Dtype=", nd.dtype,
+        #      "  Size=", nd.size)
+        np.save(weightFile, nd)
+        numWeights += 1
+    print("INFO: wrote %d weights" % numWeights)
+
+  def writeDot(self, depth, outFile, outFormat = "svg"):
+    dot = Digraph(comment="writeDot")
+    for n in self.__kg.getNodes():
+      tfOp = n.getAttr("tfop")
+      dot.node(n.getName(), tfOp.op)
+    for e in self.__kg.getEdges():
+      #print(e)
+      dot.edge(e.fromNode().getName(), e.toNode().getName())
+
+    # Add subgraphs
+    clusters = {}
+    for n in sorted(self.__kg.getNodes(), key=lambda x: x.getName()):
+      clStrs = n.getName().split("/")
+      c = clusters
+      #for i in range(0, len(clStrs)):
+      for i in range(0, min(len(clStrs), depth)):
+        if (c.get(clStrs[i]) == None):
+          c[clStrs[i]] = {"nodes" : []}
+        c = c[clStrs[i]]
+      c["nodes"].append(n.getName())
+    #print("Clusters=", clusters)
+
+    def addClusterNodes(graph, ClusterNode):
+      # add nodes in this subgraph
+      if ClusterNode.get("nodes") != None:
+        for n in ClusterNode["nodes"]:
+          #print("  DEBUG: added node ", n)
+          graph.node(n)
+      # add subgraphs
+
+      for clShortName in ClusterNode:
+        #print("  DEBUG: clShortName ", clShortName)
+        if clShortName != "nodes":
+          with graph.subgraph(name="cluster_" + clShortName) as subGraph:
+            subGraph.attr(color="blue")
+            subGraph.attr(label=clShortName)
+            addClusterNodes(subGraph, ClusterNode.get(clShortName))
+
+    if 1:
+      addClusterNodes(dot, clusters)
+
+    #print("Dot=", dot.source)
+    dot.format = outFormat
+    dot.render(outFile)
+    print("INFO: wrote " + outFile)
+    
+  
+  def tf2dotName(tfName):
+    return(re.sub(":\d+$", "", tfName))
+
