7989ea1abcf184ff048fd1dc51625d23e6f96078
Drazen Borkovic <dborkov@amazon.com>
2017-10-20 16:42:23 -0700

    Fix __init__.py for arch

--- compiler/python/state-buffer-estimate/arch/__init__.py
+++ compiler/python/state-buffer-estimate/arch/__init__.py
@@ -1,12 +1,4 @@
 """
-Module that contains classes for various NN layer types:
-
-convlayer.py - Convolution layer
-batchnormlayer.py - Batch normalization layer
-datalayer.py - Data layer (for inputs)
-layer.py - Abstract base class for all layers
-poollayer.py - Pooling layer
-relulayer.py - ReLU layer
-concatlayer.py - Concatenate two input maps. Pass through. Used in DenseNet.
+Module that contains classes for various TPB components (PE array, PSUM buffer, Activation engine, Pooling engine):
 """
 
