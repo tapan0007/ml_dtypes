08bca6d019cbe2087586002bc3665c916bdd2e9a
Jindrich Zejda <zejdaj@amazon.com>
2017-10-25 02:50:20 +0000

    Added if/ofmap generation, csv output with layer sizes and npy file names. Corrected conv node color in the svc output Implemented feedback from review CR-819510 Limitations:  - op args are in random order the csv(e.g., weights and images for conv op)

--- compiler/tffe/tffe.py
+++ compiler/tffe/tffe.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # Top level Tensorflow to Tonga compiler interface
 #
 # Examples
@@ -9,6 +11,9 @@
 #   python3 tffe.py --tfpb $tfpb --depth 5 --weights
 #
 #   python3 tffe.py --tfpb $tfpb --depth 5 --focus block3/unit_18
+#
+#   set img = ../../../Kaena-external-opensource/apps/tf/resnet_v2_152/dog.jpg
+#   ./tffe.py --tfpb $tfpb --depth 5 --images $img
 
 
 import argparse
@@ -20,6 +25,7 @@ parser.add_argument('--tfpb', help='TensorFlow freeze graph file', default="f.pb
 parser.add_argument('--out_prefix', help='Prefix for output files', default="out_")
 parser.add_argument('--weights', help='Generate weight files', dest="weights",
                     action="store_true",  default=False)
+parser.add_argument('--images', help='Generate images (IFMAP and OFMAP files) for an input image', dest="images", default=None)
 parser.add_argument('--depth', help='Depth of layer name hierarchy to show in the dot output',
                     default=5)
 parser.add_argument('--focus', help='Regular expression to filter a subset of nodes',
@@ -30,12 +36,16 @@ args = parser.parse_args()
 file = args.tfpb
 if not os.path.isfile(file):
   raise("ERROR: missing --tfpb " + file)
+if args.images != None and args.focus != ".*":
+  raise("ERROR: Unsupported --images with --focus")
 
 tffe = TfFrontEnd.TfFe()
 tffe.loadPb(file, args.focus)
 tffe.writeDot(int(args.depth), args.out_prefix + "graph.dot", "svg")
 if args.weights:
   tffe.writeWeights(args.out_prefix)
-
+if args.images != None:
+  tffe.writeImages(args.out_prefix, args.images)
+  tffe.writeOpsCsv(args.out_prefix + "ops.csv")
 
 
