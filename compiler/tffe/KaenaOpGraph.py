352731b9d9f802cb54456e7c1c50c66b40d99c73
Jindrich Zejda <zejdaj@amazon.com>
2017-10-25 02:50:20 +0000

    Added if/ofmap generation, csv output with layer sizes and npy file names. Corrected conv node color in the svc output Implemented feedback from review CR-819510 Limitations:  - op args are in random order the csv(e.g., weights and images for conv op)  - levelization is O(N**3) for simplicity; it is ok since these are tiny graphs (<< 1M nodes), will update predecessor map for total O(N**2) and successor queue for O(NlogN)

--- compiler/tffe/KaenaOpGraph.py
+++ compiler/tffe/KaenaOpGraph.py
@@ -10,10 +10,25 @@ class Object:
     return(self.__name)
   def getAttr(self, attrName):
     return(self.__attrs[attrName])
+  def setAttr(self, attrName, attrVal):
+    self.__attrs[attrName] = attrVal
 
 class Node(Object):
+  def __init__(self, name, attrs):
+    Object.__init__(self, name, attrs)
+    self.__level = -1
   def __str__(self):
     return("Node=" + self.getName())
+  def getLevel(self):
+    return(self.__level)
+  def setLevel(self, level):
+    self.__level = level
+  def getNpInfo(self):
+    return(self.getName(),
+           self.getAttr("op_type"),
+           self.getAttr("np_shape"),
+           self.getAttr("np_file"))
+           
 
 class Edge(Object):
   def __init__(self, fromNode, toNode, attrs):
@@ -45,22 +60,77 @@ class Graph(Object):
     self.__edges[fromNode][toNode] = Edge(fromNode, toNode, attrs)
 
   def hasNode(self, name):
-    return(self.__name2node.get(name) != None)
+    return(self.__name2node.get(name, None))
 
   def getNode(self, name):
     return(self.__name2node[name])
   
   def getEdge(self, nameFrom, NameTo):
-    return(self.__name2node[name])
+    return(self.__edges[nameFrom][nameTo])
   
   def getNodes(self):
-    return(self.__name2node.values())
+    return(list(self.__name2node.values()))
   
   def getEdges(self):
     edges = []
     for fromNode in self.__edges:
       edges += list(self.__edges[fromNode].values())
     return(edges)
+  
+  def nodeSuccessors(self, fromNode):
+    nextNodes = []
+    if self.__edges.get(fromNode):
+      nextNodes = list(self.__edges[fromNode].keys())
+    return(nextNodes)
+  
+  def nodePredecessors(self, toNode):
+    preNodes = []
+    for fromNode in self.__edges.keys():
+      if toNode in self.__edges[fromNode]:
+        preNodes.append(fromNode)
+    return(preNodes)
+  
+  def getTopNode(self):
+    nextNodes = self.getNodes()
+    while len(nextNodes) > 0:
+      n = nextNodes[0]
+      nextNodes = self.nodeSuccessors(n)
+    return(n)
+        
+  def levelize(self):
+    nodes = self.getNodes()
+    perDot = max(1, int(len(nodes) / 80))
+    print("INFO: levelizing ...")
+    self.__level2nodes = []
+    numLevelized = 0
+    while len(nodes) > 0:
+      n = nodes.pop(0)
+      #print("DEBUG: levelize popped node  ", n.getName())
+      level = 0
+      for preNode in self.nodePredecessors(n):
+        if preNode.getLevel() < 0:
+          #print("    DEBUG: postponed node  ", n.getName())
+          nodes.append(n)
+          level = -1
+          break
+        else:
+          level = max(level, preNode.getLevel())
+      if level >= 0:
+        level += 1
+        n.setLevel(level)
+        #print("  DEBUG: node ", n.getName(), " got level ", level)
+        while len(self.__level2nodes) <= level:
+          self.__level2nodes.append([])
+        self.__level2nodes[level].append(n)
+        numLevelized += 1
+        if numLevelized % perDot == 0:
+          print(".", end='', flush=True)
+    print("", flush=True)
+  
+  # array of node lists, index is level from leaves (inputs) to output (classify)
+  def getLevelizedNodes(self):
+    return(self.__level2nodes)
+  
 
 
 
--- compiler/tffe/TfFrontEnd.py
+++ compiler/tffe/TfFrontEnd.py
@@ -7,13 +7,15 @@
 #   https://storage.googleapis.com/download.tensorflow.org/models/inception_v3_2016_08_28_frozen.pb.tar.gz
 
 import tensorflow as tf
-import numpy as np   
+import numpy as np
 from tensorflow.python.platform import gfile
 from tensorflow.core.framework import graph_pb2
 from tensorflow.python.framework import tensor_util
 from graphviz import Digraph
 import re
 import KaenaOpGraph as kog
+from PIL import Image
+import csv
 
 class TfOp:
   def __init__(self, name, op, tfNode):
@@ -56,11 +58,12 @@ class TfFe:
     numOps = 0
     numConv = 0
     
+    # Iterate over all TF graph definition nodes
     for tfNode in self.__gd.node:
       tfop = TfOp(tfNode.name, tfNode.op, tfNode)
       #print(tfop)
       if (re.search(focusNodeRe, tfNode.name) != None):
-        self.__kg.addNode(tfNode.name, {"tfop" : tfop})
+        self.__kg.addNode(tfNode.name, {"tfop" : tfop, 'op_type' : tfNode.op})
         numOps += 1
         if (re.search("conv", tfop.op.lower(), re.I) != None):
           numConv += 1
@@ -88,11 +91,78 @@ class TfFe:
         numWeights += 1
     print("INFO: wrote %d weights" % numWeights)
 
+
+  def writeImages(self, outPrefix, imageFile):
+    inputNode = self.__kg.getNode("input")
+    self.__kg.levelize()
+    inputTfOpName = inputNode.getAttr("tfop").name
+    with tf.Session() as sess:
+      tf.import_graph_def(self.__gd, name="")
+      graph = sess.graph
+      inputOp = graph.get_operation_by_name(inputTfOpName)
+      inputTensor = inputOp.outputs[0]
+      inputShape = inputTensor.get_shape().as_list()
+      shapeXY = inputShape[1:3]
+      img = Image.open(imageFile).resize(shapeXY)
+      img = np.array(img)
+      img = img.reshape(inputShape)
+
+      tfVars = []
+      kNodes = []
+      # Levelization is not necessary of TF, but ok to use to order the np files
+      nodeQueue = [inputNode]
+      visitedNodes = {}
+      
+      levelizedNodes = self.__kg.getLevelizedNodes()
+      for level in range(0, len(levelizedNodes)):
+        for n in levelizedNodes[level]:
+          #print("DEBUG: node=", n.getName())
+          tfOpName = n.getAttr("tfop").name
+          op = graph.get_operation_by_name(tfOpName)
+          tensor = op.outputs[0]
+          shape = tensor.get_shape().as_list()
+          n.setAttr("np_shape", shape)
+          tfVars.append(tensor.name)
+          kNodes.append(n)
+          # Add support for multi-output graphs
+          assert(len(op.outputs)==1)
+      
+      print("INFO: identified %d operations, computing ..." % len(tfVars))
+      numImages = 0
+      tfResults = sess.run(tfVars, feed_dict={inputTensor.name : img})
+      perDot = max(1, int(len(tfVars) / 80))
+      print("INFO: writing if/ofmap files ...")
+      for (n, var, nd) in zip(kNodes, tfVars, tfResults):
+        if nd.size > 0:
+          imageFile = outPrefix + var.replace("/", "__")
+          #print("ImageFile=", weightFile,
+          #      "  Dtype=", nd.dtype,
+          #      "  Size=", nd.size)
+          np.save(imageFile, nd)
+          numImages += 1
+          if numImages % perDot == 0:
+            print(".", end='', flush=True)
+          n.setAttr("np_file", imageFile + ".npy")
+        else:
+          print("INFO: Failed to get tensor content for ",
+                tfOp.type, "  ", tfOp.name)
+      print("")
+    print("INFO: wrote %d i/ofmap files" % numImages)
+
+
+  # Writes graph in a given format using graphviz lib
+  # Key operations like convolution are colored red
+  # The depth determines subgraph clastering based on operation manes
+  #   layerN/branchM/convP cluster would be breated (in blue) if depth is 3
   def writeDot(self, depth, outFile, outFormat = "svg"):
     dot = Digraph(comment="writeDot")
     for n in self.__kg.getNodes():
       tfOp = n.getAttr("tfop")
-      dot.node(n.getName(), tfOp.op)
+      attrs = {}
+      if re.search("conv", tfOp.op, re.I):
+        attrs["color"] = "red"
+      dot.node(n.getName(), tfOp.op, attrs)
+
     for e in self.__kg.getEdges():
       #print(e)
       dot.edge(e.fromNode().getName(), e.toNode().getName())
@@ -135,6 +205,53 @@ class TfFe:
     print("INFO: wrote " + outFile + "." + outFormat)
     
   
+  @staticmethod
   def tf2dotName(tfName):
     return(re.sub(":\d+$", "", tfName))
+
+  @staticmethod
+  def npShapeToSize(shapeList):
+    arr = np.ndarray(shapeList)
+    return(arr.size)
+
+
+  def writeOpsCsv(self, csvFile):
+    levelizedNodes = self.__kg.getLevelizedNodes()
+    with open(csvFile, 'w') as csvHandle:
+      fieldNames = ["OpName", "OpType", "Level",
+                    "OutputData", "OutputSize", "InputSize",
+                    "OutputShape", "OutputFile"]
+      for i in range(0,6):
+        fieldNames += ["Input" + str(i) + "Shape",
+                       "Input" + str(i) + "File"]
+      writer = csv.DictWriter(csvHandle, fieldnames=fieldNames)
+
+      rows = []
+      for level in range(0, len(levelizedNodes)):
+        for n in levelizedNodes[level]:
+          #print("DEBUG: node=", n.getName())
+          (opName, opType, npShape, npFile) = n.getNpInfo()
+          row = {"OpName"      : opName,
+                 "OpType"      : opType,
+                 "Level"       : level,
+                 "OutputData"  : "TBD", 
+                 "OutputSize"  : TfFe.npShapeToSize(npShape), 
+                 "OutputShape" : npShape, 
+                 "OutputFile"  : npFile}
+          
+          i = 0
+          inputSize = 0
+          for preNode in self.__kg.nodePredecessors(n):
+            (opName, opType, npShape, npFile) = preNode.getNpInfo()
+            row["Input" + str(i) + "Shape"] = npShape
+            row["Input" + str(i) + "File"] = npFile
+            inputSize += TfFe.npShapeToSize(npShape)
+            i += 1
+          row["InputSize"] = inputSize
+          rows.append(row)
+      writer.writeheader()
+      writer.writerows(rows)
+    print("INFO: Wrote op sequences into " + csvFile)
+
+
 
