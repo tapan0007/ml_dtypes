532283f0d0fa5d0e09d5a6359cfab067ef925ab5
Rich Heaton <rjheaton@amazon.com>
2017-10-31 09:04:43 -0700

    adding check for linux to allow MAP_POPULATE to not break build when building on Mac OSX

--- sim/src/ibufferfile.h
+++ sim/src/ibufferfile.h
@@ -6,6 +6,18 @@
 #include <sys/types.h>
 #include <fcntl.h>
 
+#if __linux__
+#include <linux/version.h>
+#if LINUX_VERSION_CODE > KERNEL_VERSION(2,6,22)
+#define _MAP_POPULATE_AVAILABLE
+#endif
+#endif
+
+#ifdef _MAP_POPULATE_AVAILABLE
+#define MMAP_FLAGS (MAP_PRIVATE | MAP_POPULATE)
+#else
+#define MMAP_FLAGS MAP_PRIVATE
+#endif
 
 class IBufferFile : public UopFeedInterface {
     public:
@@ -14,14 +26,14 @@ class IBufferFile : public UopFeedInterface {
             fd = open(fname, O_RDONLY, 0);
             stat(fname, &st);
             fsize = st.st_size;
-            data_start = (char *)mmap(NULL, fsize, PROT_READ, 
-                    MAP_PRIVATE | MAP_POPULATE, fd, 0);
+            data_start = (char *)mmap(NULL, fsize, PROT_READ,
+                                      MMAP_FLAGS, fd, 0);
             data_idx = data_start;
         }
         ~IBufferFile() { free(data_start); }
         bool         empty()   {return data_idx == data_start + fsize;}
         void        *front()   {return data_idx;}
-        void         pop()     {data_idx += INSTRUCTION_NBYTES;} 
+        void         pop()     {data_idx += INSTRUCTION_NBYTES;}
     private:
         int    fd;
         size_t  fsize;
