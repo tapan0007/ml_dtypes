1ec966ed77d6263daac8c1f8dda80a8d591093eb
Jindrich Zejda <zejdaj@amazon.com>
2017-11-07 00:23:27 +0000

    Simplified TFTE weight and fmap format conversions from TF to Inkling - no functional change.

--- compiler/tffe/NpTransforms.py
+++ compiler/tffe/NpTransforms.py
@@ -1,4 +1,10 @@
 # Numpy file transformation definitions and services
+#
+# It allows to specify framework (e.g. TF) formats for weights
+# and fmaps (e.g. NHWC) and destination platform (e.g. SIM - Inlikng
+# simulator)
+# It computes the transformations upon import, and at runtime performs
+# the data format conversions.
 
 import numpy as np
 
@@ -6,20 +12,15 @@ def calcTransform(sf, st):
   assert(len(sf) == len(st))
   transform = [sf.find(c) for c in list(st)]
   #print("DEBUG: transform=", transform)
-  assert(transform[0] >= 0)
+  assert(all(i >=0 for i in transform))
   return(transform)
 
 class NpTrans:
   # See spec for method  genCompilerPy
-  TF = "TF"
-  SIM = "SIM"
-  TF2SIM = "TF2SIM"
-  Fmaps   = "Fmaps"
-  Weights = "Weights"
-  NHWC = "NHWC"
-  NCHW = "NCHW"
-  RSCM = "RSCM"
-  MCRS = "MCRS"
+  for c in ["TF", "SIM", "Fmaps", "Weights", "NHWC", "NCHW", "RSCM", "MCRS"]:
+    exec("%s = '%s'" %(c, c))
+  
+  # Define tensorFlow (TF) to Inkling simulator (SIM) translation
   Formats = {
     TF : {
       Fmaps   : NHWC,
@@ -30,18 +31,21 @@ class NpTrans:
       Weights : MCRS
       }
     }
-  Transforms = {
-    TF2SIM : {
-      Fmaps   : calcTransform(Formats[TF][Fmaps],   Formats[SIM][Fmaps]),
-      Weights : calcTransform(Formats[TF][Weights], Formats[SIM][Weights])
-      }
-    }
-  # Ulility function to convert npy files, can be moved out of the graph later
+  Transforms = {}
+  for src in [TF, SIM]:
+    Transforms[src] = {}
+    for dst in [TF, SIM]:
+      Transforms[src][dst] = {}
+      for d in [Fmaps, Weights]:
+        Transforms[src][dst][d] = calcTransform(Formats[src][d], Formats[dst][d])
+  # Ulility function to convert npy files, returns new file name and the destination format
   @staticmethod
-  def copyNpyFileAs(npFile, destFormat, transform):
+  def copyNpyFileAs(npFile, srcPlat, dstPlat, dataFlavor):
+    dstFormat = NpTrans.Formats[dstPlat][dataFlavor]
+    transform = NpTrans.Transforms[srcPlat][dstPlat][dataFlavor]
     arr = np.load(npFile)
     arr = np.transpose(arr, transform)
-    npFileDest = npFile.replace(".npy", "_" + destFormat + ".npy")
+    npFileDest = npFile.replace(".npy", "_" + dstFormat + ".npy")
     np.save(npFileDest, arr)
-    return(npFileDest)
+    return(npFileDest, dstFormat)
 
