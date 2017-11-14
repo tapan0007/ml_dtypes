9cdbf93770701ffad67a307764df07061dc5dc67
Drazen Borkovic <dborkov@amazon.com>
2017-11-14 11:35:22 -0800

    Improve RunOne.sh to use committed dumpnpy

--- compiler/python/state-buffer-estimate/test/RunOne.sh
+++ compiler/python/state-buffer-estimate/test/RunOne.sh
@@ -1,3 +1,5 @@
+SCRIPT=$0
+
 Error () {
     echo 1>&2 "$@"
 }
@@ -5,6 +7,10 @@ Fatal () {
     Error '>>>' "$@"
     exit 1
 }
+FatalUsage () {
+    Fatal Usage: $SCRIPT TGZ-FILE
+}
+
 ##############################################################
 case "x$INKLING_PATH" in
 (x) INKLING_PATH=$HOME/code/CodeCommit/Inkling ;;
@@ -17,6 +23,7 @@ esac
 export COMPILER=$KAENA_PATH/compiler/python/state-buffer-estimate
 export TEST=$COMPILER/test
 export npy_diff=$KAENA_PATH/compiler/util/npy_diff_files
+export DUMPNPY=$KAENA_PATH/compiler/util/dumpnpy
 export INKLING=$INKLING_PATH
 
 export PYTHONPATH=$PYTHONPATH:$COMPILER
@@ -34,11 +41,15 @@ RunCmd () {
 
 ##############################################################
 case $# in
-(0) Fatal Usage $0 RESULT;;
+(0) FatalUsage;;
 esac
 
 case "x$1" in
-(x*.tgz) TGZ=$1; Name=${TGZ%.tgz};;
+(x*.tgz)
+    TGZ=$1; 
+    x=${TGZ%.tgz}
+    Name="${x#.*/}"
+    ;;
 (*) Name=$1; TGZ=$Name.tgz;;
 esac
 
@@ -47,14 +58,24 @@ DIR=./results
 SIM=$INKLING/sim/sim
 OBJDUMP=$INKLING/objdump/objdump
 
-NET=trivnet
+##############################################################
+tar xvfz $TGZ
+#PYTHONPATH="$PYTHONPATH:$PWD"
+. ./files.sh 
+sed1='s/.*":  *"//'; sed2='s/",.*//'
+
+InputNpy=$( egrep '"input_file":' $JsonFile | sed -e "$sed1" -e "$sed2" )
+NetName=$( egrep '"net_name":' $JsonFile | sed -e "$sed1" -e "$sed2" )
+LastLayerName=$( egrep '"layer_name":' $JsonFile | tail -n 1| sed -e "$sed1" -e "$sed2" )
+
+NET=$NetName
 
 CPP=$NET.cpp
 OBJ=$NET.o
 EXE=$NET-exe
 
 RESULTS=./results/$Name
-rm -fR $RESULTS; mkdir $RESULTS || Fatal Cannot mkdir dir $DIR
+rm -fR $RESULTS; mkdir -p $RESULTS || Fatal Cannot mkdir dir $DIR
 
 ASM=$RESULTS/$NET.asm
 TPB=$RESULTS/$NET.tpb
@@ -65,13 +86,12 @@ LOG=$RESULTS/LOG
 
 ##############################################################
 {
-tar xvfz $TGZ
-PYTHONPATH="$PYTHONPATH:$PWD"
+
 
 ##############################################################
 ## First make NET.py file
-touch __init__.py
-RunCmd python3 $COMPILER/main.py --trivnet
+#touch __init__.py
+RunCmd python3 $COMPILER/main.py --json $JsonFile
 cp -p $CPP $RESULTS/.
 
 ##############################################################
@@ -84,11 +104,11 @@ LDFLAGS="$FLAGS -ltcc"
 LIBDIR1="$INKLING/tcc/libs"
 LIBDIR_FLAGS="-L$LIBDIR1"
 
-g++ $CPPFLAGS -c "$CPP" || Fail Failed to compile $CPP
-g++ $LDFLAGS -o $EXE $OBJ $LIBDIR_FLAGS $LIB_FLAGS || Fail Failed to link $OBJ
+g++ $CPPFLAGS -c "$CPP" || Fatal Failed to compile $CPP
+g++ $LDFLAGS -o $EXE $OBJ $LIBDIR_FLAGS $LIB_FLAGS || Fatal Failed to link $OBJ
 
 ##############################################################
-RunCmd ./$EXE $TPB || Fatal Failed $EXE $TPB
+RunCmd ./$EXE $TPB || Fatal Failed ./$EXE $TPB
 
 ##############################################################
 RunCmd $OBJDUMP $TPB > $ASM || Fatal Failed to create $ASM
@@ -98,14 +118,14 @@ RunCmd shasum $TPB
 echo $SIM $TPB
 $SIM $TPB >$SIMRES || Fatal Sim failed on $TPB
 
+SimOutputNpy="$NetName-$LastLayerName-simout.npy"
+SimOutputNpy="$(echo $SimOutputNpy | sed -e 's@/@-@g')"
 ##############################################################
-outNpy=$( egrep -A 1 compile_write_ofmap $CPP |
-            tail -n 1 | sed -e 's/^ *"//' -e 's/.npy".*//' )
-echo Out npy: $outNpy
+echo Out npy: $OutputNpy
 
-Files="
-    output
-    $outNpy
+NpyFiles="
+    $OutputNpy
+    $SimOutputNpy
 "
     # int16
 
@@ -114,26 +134,27 @@ FMTS="
 "
 
 for f in $Files; do
-    cp $f.npy $RESULTS/.
+    cp $f $RESULTS/.
 done
 
-echo $npy_diff output.npy $outNpy.npy '>' $RESULTS/$NET.diff
-$npy_diff output.npy $outNpy.npy 2>&1 | tee $RESULTS/$NET.diff
+echo $npy_diff $OutputNpy $SimOutputNpy '>' $RESULTS/$NET.diff
+$npy_diff $OutputNpy $SimOutputNpy 2>&1 | tee $RESULTS/$NET.diff
+
 
 for fmt in $FMTS; do
-    for file in $Files; do
-        npy=$file.npy
-        txt=$RESULTS/$file-$fmt.txt
-        echo dumpnpy --$fmt $npy '>' $txt
-        dumpnpy --$fmt $npy > $txt
+    for npyFile in $NpyFiles; do
+        txt=$RESULTS/$npyFile-$fmt.txt
+        echo $DUMPNPY --$fmt $npyFile '>' $txt
+        $DUMPNPY --$fmt $npyFile > $txt
         shasum  $txt
         #cat $txt
     done
 done
 
 #echo GOLDEN:
-#cat output-float16.txt
+#cat $OutputNpy-float16.txt
 #echo FROM SIM:
-#cat $NET-out-float16.txt
+#cat $SimOututNpy-float16.txt
 
 } 2>&1 | tee $LOG
+
