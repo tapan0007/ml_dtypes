a4ab8d60183033e82882bef0ac9ba5f3bee1db82
ilya <imminkin@amazon.com>
2018-02-14 14:51:09 -0800

    cleaned up plat and platfrom files

--- runtime/hal/file_list_base.mk
+++ runtime/hal/file_list_base.mk
@@ -8,23 +8,37 @@ HAL_UDMA_SOURCES = \
 
 # TODO need this? 	$(HAL_TOP)/src/udma/al_hal_msg_ipc.c	\
 
+HAL_IOFIC_SOURCES = \
+    $(HAL_TOP)/src/iofic/al_hal_iofic.c \
+
 HAL_TPB_SOURCES = \
     $(HAL_TOP)/src/tpb/aws_hal_tpb_pe.c \
     $(HAL_TOP)/src/tpb/aws_hal_tpb_pool.c \
     $(HAL_TOP)/src/tpb/aws_hal_tpb.c \
 
-HAL_SOURCES = $(HAL_UDMA_SOURCES) $(HAL_TPB_SOURCES)
+#HAL_PLAT_SOURCES = \
+#    $(HAL_TOP)/src/plat/dummy/dummy_plat_services.c \
+
+HAL_SOURCES = $(HAL_UDMA_SOURCES) $(HAL_TPB_SOURCES) $(HAL_PLAT_SOURCES) $(HAL_IOFIC_SOURCES)
 
 #sources of the init files compiled in the HAL itself
 #HAL_INIT_SOURCES_GENERIC = \
 #	$(HAL_TOP)/services/err_events/al_err_events_udma.c \
 
 #include path that a HAL user needs
-HAL_INCLUDE_PATH = \
+
+# platform IOMAP, should be generated from something ???
+HAL_PLATFORM_INCLUDE = \
+    -I$(HAL_TOP)/platform
+
+HAL_MAIN_INCLUDE = \
 	-I$(HAL_TOP)/include/common \
 	-I$(HAL_TOP)/include/udma \
 	-I$(HAL_TOP)/include/iofic \
-    -I$(HAL_TOP)/include/plat_api
 
+HAL_PLAT_INCLUDE = \
+    -I$(HAL_TOP)/include/plat/dummy \
+
+HAL_INCLUDE_PATH = $(HAL_MAIN_INCLUDE) $(HAL_PLAT_INCLUDE) $(HAL_PLATFORM_INCLUDE)
 #	-I$(HAL_TOP)/include/udma_fast \
 
