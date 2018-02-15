Fix max pool routine to add padding for stride != pooling window; fix pooling ofmap_full_tilex_sz computation for the case when E is smaller than pooling window

compiler/util/layeropt.py
@@ -93,18 +93,33 @@ class Pool:
         tile_array = in_array.reshape(ofmap_tilex_sz,ofmap_tiley_sz,Tn,num_cols)
         window_shape = (pool_window_size,pool_window_size,Tn,num_cols)
         stride_shape = (stride, stride, Tn, 1)
-        pool_result = view_as_windows(tile_array,window_shape,stride_shape).mean(axis=(4,5)).reshape(-1,num_cols)
+        #pool_result = view_as_windows(tile_array,window_shape,stride_shape).mean(axis=(4,5)).reshape(-1,num_cols)
+        pool_result_temp = view_as_windows(tile_array, window_shape, stride_shape)
+        pool_result = pool_result_temp.mean(axis=(4,5)).reshape(-1,num_cols)
         return pool_result
     def resadd(self, array_a, array_b):
         return array_a + array_b
-    def max(self, in_array, stride, pool_window_size, Tn, ofmap_tilex_sz, ofmap_tiley_sz):
+    def max(self, in_array, stride, pool_window_size, Tn, ifmap_tilex_sz, ifmap_tiley_sz, ofmap_tilex_sz, ofmap_tiley_sz):
         num_cols = in_array.shape[1]
         # view_as_windows needs in_array to be in the same dimension as window_shape
         # need to make sure the third dimension of stride_shape to be '1' since that is the column direction
-        tile_array = in_array.reshape(ofmap_tilex_sz, ofmap_tiley_sz, Tn, num_cols)
-        window_shape = (pool_window_size, pool_window_size, Tn, num_cols)
-        stride_shape = (stride, stride, Tn, 1)
-        pool_result = view_as_windows(tile_array, window_shape, stride_shape).max(axis=(4,5)).reshape(-1,num_cols)
+        input_tilex_with_pad = ifmap_tilex_sz + pool_window_size - stride
+        input_tiley_with_pad = ifmap_tiley_sz + pool_window_size - stride
+        input_tile_with_pad_sz = input_tilex_with_pad*input_tiley_with_pad
+        tile_array = np.zeros((input_tilex_with_pad, input_tiley_with_pad))
+        pool_result = np.zeros((ofmap_tilex_sz*ofmap_tiley_sz, num_cols))
+        for i in range(num_cols):
+            tile_array[0:ifmap_tilex_sz, 0:ifmap_tiley_sz] = in_array[0:ifmap_tilex_sz*ifmap_tiley_sz,i].reshape(ifmap_tilex_sz, ifmap_tiley_sz) # ignoring Tn for now
+            window_shape = (pool_window_size, pool_window_size)
+            stride_shape = (stride, stride)
+            pool_result_temp = view_as_windows(tile_array, window_shape, stride_shape)
+            pool_result[:,i] = pool_result_temp.max(axis=(2,3)).reshape(-1)
+            if (i==0):
+                print("tile_array :", tile_array)
+                print("window_shape :", window_shape)
+                print("stride_shape :", stride_shape)
+                print("pool_result_temp :", pool_result_temp)
+                print("pool_result :", pool_result)
         return pool_result
 
 ##################################################################################
@@ -211,7 +226,13 @@ class CircularBuffer:
         #   * different FMAPs folds will be in different atoms (for now)
         # TODO: refactor the following function since it is used in multiple places
         if (self.layer_type == 'Input' or self.layer_type == 'Const' or self.layer_type == 'Output'):
-            N, C, H, W = self.dram_data.shape
+            if (self.layer_format == 'NCHW'):
+                N, C, H, W = self.dram_data.shape
+            elif (self.layer_format == 'CNHW'):    
+                C, N, H, W = self.dram_data.shape
+            else:
+                print("ERROR: Unrecognized layer %s format %s"%(self.layer_name, self.layer_format))
+                exit(-1)
             self.dram_data_len_per_NC = self.dram_data_len//(N*C)
             ifmap_data_len = H * W * self.item_sz
             ifmap_width_data_len = W * self.item_sz
@@ -432,7 +453,7 @@ class KNode:
         # If the EF is large, we need to make sure tiley is at least the same size as the pool_window
         #if ((self.EF > PEArray.MAX_WAVE_SIZE) and adjust_for_pool):
         if (adjust_for_pool and self.ofmap_full_tiley_sz < self.pool_window_y):
-            self.ofmap_full_tiley_sz = self.pool_window_y
+            self.ofmap_full_tiley_sz = min(self.E, self.pool_window_y)
             self.ofmap_full_tilex_sz = min(self.F * self.Tn, PEArray.MAX_WAVE_SIZE // self.ofmap_full_tiley_sz)
             #self.ofmap_full_tilex_sz = PEArray.MAX_WAVE_SIZE // self.ofmap_full_tiley_sz
         self.ofmap_full_tile_sz = self.ofmap_full_tilex_sz * self.ofmap_full_tiley_sz
@@ -440,8 +461,6 @@ class KNode:
         self.c = ceildiv(self.C, PEArray.NUM_ROWS)
         # compute the OFMAP folds
         self.m = ceildiv(self.M, PEArray.NUM_COLS)
-        # compute the IFMAP folds over NUM_COLS
-        self.d = ceildiv(self.C, PEArray.NUM_COLS)
         # computing the input map tiling       
         self.h, self.w, self.e, self.f = 1, 1, 1, 1
         # compute ofmap folding
@@ -451,8 +470,8 @@ class KNode:
         # heigh/width folding is the same for IFMAP and OFMAP            
         self.h = self.e
         self.w = self.f
-        print("Common params for layer %s:  N=%d, M=%d, H=%d, W=%d, C=%d, E=%d, F=%d, stride_x=%d, stride_y=%d, ofmap_full_tilex_sz=%d, ofmap_full_tiley_sz=%d"
-                %(self.data['layer_name'], self.N, self.M, self.H, self.W, self.C, self.E, self.F, self.stride_x, self.stride_y, self.ofmap_full_tilex_sz, self.ofmap_full_tiley_sz))
+        print("Common params for layer %s:  N=%d, M=%d, H=%d, W=%d, C=%d, E=%d, F=%d, stride_x=%d, stride_y=%d, ofmap_full_tilex_sz=%d, ofmap_full_tiley_sz=%d, ofmap_full_tile_sz=%d"
+                %(self.data['layer_name'], self.N, self.M, self.H, self.W, self.C, self.E, self.F, self.stride_x, self.stride_y, self.ofmap_full_tilex_sz, self.ofmap_full_tiley_sz, self.ofmap_full_tile_sz))
 
     # Compute Conv looping params
     def populate_conv_params(self):
@@ -535,16 +554,16 @@ class KNode:
     #   wave_id: current wave ID, [n_id, m_id, h_id, w_id, c_id, r_id, s_id]
     #   layer_type: 'conv' or 'MaxPool'; can be extended to handle other layer types
     #   return: a 256x128 array
-    def pack_wave_ifmaps(self, ifmaps, wave_id, layer_type):
+    def pack_wave_ifmaps(self, ifmaps, wave_id, for_unfused_pooling):
         # If we are not doing convolution (aka pooling), set out_array_dim_y to be PEArray.NUM_COLS to match pooling/activation engines dimension
-        if (layer_type == 'Conv'):
-            out_array_dim_y = PEArray.NUM_ROWS
-        elif (layer_type == 'MaxPool'):
+        if (for_unfused_pooling):
             out_array_dim_y = PEArray.NUM_COLS
-        else:
-            print("ERROR: Unrecognized layer type in pack_wave_ifmaps: ",layer_type)
-            exit(-1)
-
+            fmap_folding_idx = wave_id.m_id
+            fmap_total_count = self.M
+        else:            
+            out_array_dim_y = PEArray.NUM_ROWS
+            fmap_folding_idx = wave_id.c_id
+            fmap_total_count = self.C
         out_array = np.zeros((PEArray.MAX_WAVE_SIZE, out_array_dim_y))
         # remember to extract IFMAPs starting at r_id, s_id (which should be zero for non-conv op)
         # also need to add zeros for padding
@@ -553,8 +572,9 @@ class KNode:
         self.ofmap_wave_lower_coord = (0, 0)
         self.ofmap_wave_upper_coord = (0, 0)
         self.psum_bank_offset = 0
-        pe_row_start = wave_id.c_id * out_array_dim_y
-        pe_row_stop = min(self.C, pe_row_start + out_array_dim_y)
+        # for pooling, the "row" below actually means output columns
+        pe_row_start = fmap_folding_idx * out_array_dim_y
+        pe_row_stop = min(fmap_total_count, pe_row_start + out_array_dim_y)
         assert(pe_row_start < pe_row_stop)
         for row in range(pe_row_start, pe_row_stop):
             #out_array[:,row] = self.pack_wave_ifmap(ifmaps[:, wave_id.c_id * out_array_dim_y + row], wave_id)
@@ -801,7 +821,7 @@ class FusedOp(list):
     # execute PEArray matrix multiply; returns True if successful (IFMAP wave is non-zero)
     def execute_matmul_waveop(self, tpb, wave_id, inputs, weights, psum_add):
         pearray_packed_weights = self.conv_op.pack_wave_conv_weights(weights, wave_id)
-        pearray_packed_ifmaps = self.conv_op.pack_wave_ifmaps(inputs, wave_id, 'Conv')
+        pearray_packed_ifmaps = self.conv_op.pack_wave_ifmaps(inputs, wave_id, for_unfused_pooling=False)
         #print("\npearray_packed_ifmaps", wave_id.show(), "\n", pearray_packed_ifmaps)
         #print("\npearray_packed_weights", wave_id.show(), "\n", pearray_packed_weights)
         if (self.conv_op.ifmap_wave_lower_addr < 0 or self.conv_op.ifmap_wave_upper_addr < 0):
@@ -879,15 +899,15 @@ class FusedOp(list):
             elif ((layer_type == 'AvgPool') or (layer_type == 'MaxPool')):
                 tpb.activate.wait_tile_done(tile_id)
                 self[i].compute_ofmap_tile_info(tile_id)
-                tilex = self.conv_op.tile_width
-                tiley = self.conv_op.tile_height
-                #tilex = self[i].ofmap_full_tilex_sz * self[i].stride_x
-                #tiley = self[i].ofmap_full_tiley_sz * self[i].stride_y
+                #tilex = self.conv_op.tile_width
+                #tiley = self.conv_op.tile_height
+                tilex = self[i].ofmap_full_tilex_sz * self[i].stride_x + (self[i].pool_window_x - self[i].stride_x)
+                tiley = self[i].ofmap_full_tiley_sz * self[i].stride_y + (self[i].pool_window_y - self[i].stride_y)
                 if (layer_type == 'AvgPool'):
                     psum_temp = tpb.pool.avg(psum_temp, self[i].stride_x, self[i].pool_window_y, self[i].Tn, tilex, tiley)
                 else:
-                    psum_temp = tpb.pool.max(psum_temp, self[i].stride_x, self[i].pool_window_y, self[i].Tn, tilex, tiley)
-                tpb.gen_pool_waveop_inline(op_list, tile_id, psum_bank_src)
+                    psum_temp = tpb.pool.max(psum_temp, self[i].stride_x, self[i].pool_window_y, self[i].Tn, tilex, tiley, self[i].ofmap_full_tilex_sz, self[i].ofmap_full_tiley_sz)
+                tpb.gen_fused_pool_waveop_inline(op_list, tile_id, psum_bank_src)
                 # Don't go to back to psum for pooling
                 #psum_bank_dst = 3
                 #if (i != len(op_list)-1):
@@ -1133,8 +1153,12 @@ class TPBSched:
             }
         self.waveop_stream.add_linked(instr, [])
 
-    def gen_pool_waveop_inline (self, op_list, tile_id, psum_bank_src):
+    def gen_fused_pool_waveop_inline (self, op_list, tile_id, psum_bank_src):
         pool_identity_waveop = op_list.gen_pool_waveop(self, tile_id, True, psum_bank_src)
+        self.waveop_stream.add_linked(pool_identity_waveop, [])
+
+    def gen_unfused_pool_waveop_inline (self, op_list, tile_id):
+        pool_identity_waveop = op_list.gen_pool_waveop(self, tile_id, False, 0)
         self.waveop_stream.add_linked(pool_identity_waveop, [])
 
     # Execute an unfused pooling operator
@@ -1154,22 +1178,34 @@ class TPBSched:
                 for h_id in range(op_list[0].h):
                     for w_id in range(op_list[0].w):
                         tile_id = TileID(n_id, m_id, h_id, w_id)
-                        # loops for constructing a tile
-                        # re-calculate 'c' because it needs to fit to pool engine width
-                        for c_id in range(op_list[0].d):
-                            # set r_id and s_id to zero since we are not doing convolution
-                            wave_id = WaveID(n_id, m_id, h_id, w_id, c_id, 0, 0)
-                            # need to use the conv_node to extract the ifmaps
-                            psum_fake = conv_node.pack_wave_ifmaps(inputs, wave_id, 'MaxPool')
-                            print (psum_fake)
-                            tiley = conv_node.ofmap_full_tiley_sz
-                            tilex = conv_node.ofmap_full_tilex_sz
-                            psum_fake_extract = psum_fake [0:tiley*tilex, :]
-                            psum_temp = self.pool.max(psum_fake_extract, op_list[0].stride_x, op_list[0].pool_window_y, op_list[0].Tn, tilex, tiley)
-                            np.save("debug.npy",psum_fake_extract)
-                            print (op_list[0].pool_window_y, "", op_list[0].stride_x)
-                            print (psum_fake_extract[:,0])
-                            print (psum_temp[:,0])
+                        # set r_id and s_id in wave_id to zero since we are not doing convolution
+                        wave_id = WaveID(n_id, m_id, h_id, w_id, 0, 0, 0)
+                        # need to use the conv_node to extract the ifmaps
+                        psum_fake = conv_node.pack_wave_ifmaps(inputs, wave_id, for_unfused_pooling=True)
+                        input_tiley = conv_node.ofmap_full_tiley_sz
+                        input_tilex = conv_node.ofmap_full_tilex_sz
+                        output_tiley = op_list[0].ofmap_full_tiley_sz
+                        output_tilex = op_list[0].ofmap_full_tilex_sz
+                        psum_fake_extract = psum_fake [0:input_tiley*input_tilex, :]
+                        psum_temp = self.pool.max(psum_fake_extract, op_list[0].stride_x, op_list[0].pool_window_y, op_list[0].Tn, input_tilex, input_tiley, output_tilex, output_tiley)
+                        #tpb.gen_unfused_pool_waveop_inline(op_list, tile_id)
+                        #np.save("debug.npy",psum_fake_extract)
+                        #print (op_list[0].pool_window_y, "", op_list[0].stride_x)
+                        #print (psum_fake_extract)
+                        #print (psum_temp)
+                        #print(op_list[0].ofmap_full_tile_sz)
+                        for j in range(PEArray.NUM_COLS):
+                            M_idx = wave_id.m_id * PEArray.NUM_COLS + j
+                            if (M_idx >= op_list[0].M):
+                                break
+                            else:
+                                # For now, multiply zeros, and at the ofmap, extract tile with zeros, then clip
+                                result_tile = (psum_temp[0 : op_list[0].ofmap_full_tile_sz, j]).reshape((op_list[0].ofmap_full_tiley_sz, op_list[0].ofmap_full_tilex_sz))
+                                result[n_id, 
+                                        j, 
+                                        op_list[0].tile_y_start : op_list[0].tile_y_start + op_list[0].tile_height, 
+                                        op_list[0].tile_x_start : op_list[0].tile_x_start + op_list[0].tile_width]\
+                                    = result_tile[0:op_list[0].tile_height, 0:op_list[0].tile_width]
         return result
 
 