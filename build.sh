65a83f1983177dbc9a099dfc0b3d25b662ce26f9
Taemin Kim <taemk@amazon.com>
2018-06-15 19:45:21 +0000

    Added 'clean' argument to wave-checker/scr/build.sh    1. This will fix a problem that wave-checker is not build when wc-build directory exists
    Change-Id: I1a70372a5b55c2afe6f72fc80ca1a453410f0d95

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd $KAENA_PATH && make "$@" || exit 1
-cd $KAENA_PATH/compiler/wave-checker && ./scr/build.sh || exit 1
+cd $KAENA_PATH/compiler/wave-checker && ./scr/build.sh clean && ./scr/build.sh || exit 1
--- compiler/wave-checker/scr/build.sh
+++ compiler/wave-checker/scr/build.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 WC_PATH=${KAENA_PATH}/compiler/wave-checker
 WC_BUILD_DIR=wc-build
-mkdir ${WC_BUILD_DIR} || exit 1
-cd ${WC_BUILD_DIR} || exit 1
-${WC_PATH}/scr/gtest-build.sh || exit 1
-cmake ${WC_PATH} -DGTEST_DIR=`realpath ./gtest-build-dir` || exit 1
-make || exit 1
+#if test $1 -eq clean_command; then
+if [ "$1" = "clean" ]; then
+  if test -d ${WC_BUILD_DIR}; then
+    \rm -rf ${WC_BUILD_DIR} || exit 1
+    echo "INFO::${WC_BUILD_DIR} directory is completely deleted" || exit 1
+  fi
+  if test -f ${WC_PATH}/CMakeCache.txt; then
+    \rm ${WC_PATH}/CMakeCache.txt || exit 1
+    echo "INFO::${WC_PATH}/CMakeCache.txt is deleted" || exit 1
+  fi
+  if test -d ${WC_PATH}/CMakeFiles; then
+    \rm -rf ${WC_PATH}/CMakeFiles || exit 1
+    echo "INFO::${WC_PATH}/CMakeFiles directory is completely deleted" || exit 1
+  fi
+else
+  mkdir ${WC_BUILD_DIR} || exit 1
+  cd ${WC_BUILD_DIR} || exit 1
+  ${WC_PATH}/scr/gtest-build.sh || exit 1
+  cmake ${WC_PATH} -DGTEST_DIR=`realpath ./gtest-build-dir` || exit 1
+  make || exit 1
+fi
