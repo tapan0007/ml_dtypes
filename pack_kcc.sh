2742147168602cc18342a66d460a4a5d7e1a542b
Willis Wang <wangwil@amazon.com>
2018-07-03 18:03:52 +0000

    added build folder clearning and output package to build/kcc
    Change-Id: I2bda21176fb34f2af1cf2c2f3b81630b1cc32042

--- pack_kcc.sh
+++ pack_kcc.sh
@@ -4,17 +4,21 @@ SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 (source $SOURCE_DIR/../shared/set_env.sh) || (>&2 echo "'shared/set_env.sh' does not exist or script in incorrect directory" && exit 1)
 
-# create installation files and directory
-mkdir -p $KCC_INSTALL_DIR || exit 1
-(cd $KAENA_PATH && make install PREFIX=$KCC_INSTALL_DIR) || exit 1
-
 # get version and patch
 cd $KAENA_PATH || exit 1
 version=$(cat $KAENA_PATH/version 2> /dev/null || (echo 0.1.0 > $KAENA_PATH/version && cat $KAENA_PATH/version))
 patch=$(git rev-list $(git rev-list --no-walk --max-count=1 --tags 2> /dev/null)..HEAD --count 2> /dev/null)
 
+# create installation files and directory
+rm -rf $KCC_INSTALL_DIR
+mkdir -p $KCC_INSTALL_DIR || exit 1
+(cd $KAENA_PATH && make install PREFIX=$KCC_INSTALL_DIR) || exit 1
+
+
 # pack installation files into one file
-tar -pcvzf ${SOURCE_DIR}/kcc-${version}-${patch}.tar.gz -C $KCC_INSTALL_DIR/.. kcc
+mkdir -p ${KCC_BLD_DIR} || exit 0
+cd ${KCC_INSTALL_DIR}
+tar -pcvzf ${KCC_BLD_DIR}/kcc-${version}-${patch}.tar.gz bin lib
 
 # clean the created directories
 rm -r $KCC_INSTALL_DIR
