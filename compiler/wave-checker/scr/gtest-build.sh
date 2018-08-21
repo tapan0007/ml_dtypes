fa09dd6c98ca886c3af451d1e9f2fd15ba33d68e
Taemin Kim <taemk@amazon.com>
2018-08-21 20:37:43 +0000

    Fixed a bug in $KAENA_PATH/compiler/wave-checker/scr/gtest-build.scr   - Made PATH absolute using realpth
    Change-Id: Id6c5ad2aeae8c3b48924f2af4853b4b0cc6abd1f

--- compiler/wave-checker/scr/gtest-build.sh
+++ compiler/wave-checker/scr/gtest-build.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/google/googletest.git gtest || exit 1
 mkdir gtest-build-dir || exit 1
 cd gtest-build-dir || exit 1
-cmake -DCMAKE_INSTALL_PREFIX:PATH=. ../gtest || exit 1
+cmake -DCMAKE_INSTALL_PREFIX:PATH=`realpath .` ../gtest || exit 1
 make || exit 1
 make install || exit 1
 cd .. || exit 1
