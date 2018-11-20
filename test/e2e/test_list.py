c0aeec61afe1b8af558b3bd595156bfd47b93be2
Vignesh Vivekraja <vigviv@amazon.com>
2018-11-20 00:17:44 +0000

    resolve merge conflict
    Change-Id: I2b71429260c6ff9ae2b06bb746069bf89d3828ec
    Signed-off-by: Vignesh Vivekraja <vigviv@amazon.com>

--- test/e2e/test_list.py
+++ test/e2e/test_list.py
@@ -1018,13 +1018,14 @@ testConfigMap = {
   ],
   
   #WaveRNN Tests #cb-compiler bug workaround - no support for constants to Sub and Mul operators - WA to remove sub operatoins in sigmoid and change Mul constants to placeholders
-  "2-wavernn_tf_ts0_wave1_host02_cb"     : [
+  "2-wavernn_tf_ts0_w1_h02_cb"     : [
     #"tf_pb", "wavernn/wave_rnn_ts0_cb_f_opt.pb","wavernn",
-    "tf_s3", "s3://kaena-nn-models", "wave_rnn_ts0_cb_f_opt.pb",
-    "--input_node prev cond init_state --depth -1 --focus_to multinomial/Multinomial  --show_op_name_in_kgraph "
+    "tf_s3", "s3://kaena-nn-models", "wavernn_ts1_cb_seed1_f_opt.pb",
+    "--input_node prev cond init_state --depth -1 --focus_to Softmax  --show_op_name_in_kgraph "
+    #"--input_node prev cond init_state --depth -1 --focus_to add_13 multinomial/Multinomial  --show_op_name_in_kgraph "
     "--images $KAENA_EXT_PATH/apps/tf/wavernn/prev_samp.npy $KAENA_EXT_PATH/apps/tf/wavernn/cond.npy $KAENA_EXT_PATH/apps/tf/wavernn/init_state.npy "
-    " --executors host 0 2 wave 1 --scheduler wave2 --schedule_options ' --nname=generic' --wavegraph_checks structure data-race --parallel_streams --partition from_multi MatMul,MatMul_2,MatMul_4,MatMul_3,MatMul_1,Mul,Mul_1 Softmax ",
-    "--input_files prev:0=$KAENA_EXT_PATH/apps/tf/wavernn/prev_samp.npy cond:0=$KAENA_EXT_PATH/apps/tf/wavernn/cond.npy init_state:0=$KAENA_EXT_PATH/apps/tf/wavernn/init_state.npy --diff_options '--tolerance 30 1e-5' "
+    " --executors host 0 2 wave 1 --scheduler qemu_wave2 --schedule_options ' --nname=generic' --wavegraph_checks structure data-race --parallel_streams --partition from_multi MatMul,MatMul_2,MatMul_4,MatMul_3,MatMul_1,Mul,Mul_1 Softmax ",
+    "--input_files prev:0=$KAENA_EXT_PATH/apps/tf/wavernn/prev_samp.npy cond:0=$KAENA_EXT_PATH/apps/tf/wavernn/cond.npy init_state:0=$KAENA_EXT_PATH/apps/tf/wavernn/init_state.npy "
   ],  
 
   "5-transformer-encoder": [ 
@@ -1211,10 +1212,9 @@ testWaiver = [
     # Transformer
     # Comment calling kp.reportOpAndSizes() to see ME failure
     ['0-transformer-matmul', 'WAIVE_KAENA964'],
-    ['0-transformer-mul', 'WAIVE_KAENA964'],
-    #wavernn
-    ['2-wavernn_tf_ts0_wave1_host02_cb','WAIVE_KAENA_WAVRNN'],
-    ['5-transformer-encoder', 'WAIVE_KAENA974'],
+    ['5-transformer-encoder', 'WAIVE_KAENA974']
+   
+
   ]
 
 noGpuTestWaiver = [
