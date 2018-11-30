4aa51d15e5ab05839e6ca6d2d769474f52f0fa56
Haichen "HC" Li <lhaiche@amazon.com>
2018-11-30 05:05:01 +0000

    [kaena-907] fixed issue in test_list_uint8 to cause tests not really tested
    Change-Id: I0e24bcaec9d3abdd4733c2ce6f3f3fdd616912de
    Signed-off-by: Haichen "HC" Li <lhaiche@amazon.com>

--- test/e2e/test_list_uint8.py
+++ test/e2e/test_list_uint8.py
@@ -8,10 +8,6 @@
 #   Name can be composed of anything - 0 can be used as base/existence test
 #   Columns: (nnStruct, nnConfig, nnLabel, nnArgs, rtArgs)
 
-conv_ba_relu_uint8_compiler_options = \
-"--executors host 0 2 wave 1 --scheduler wave2  "
-"--schedule_options ' --nname=generic --save_layer_output ' " # me options
-"--partition from_multi 'conv_ba_relu_uint8/quantized_conv2d' 'conv_ba_relu_uint8/output' "
 
 testConfigMap = {
     # Need rather high tolerance for now since TensorFlow's quantize/dequantize
@@ -20,31 +16,41 @@ testConfigMap = {
         "trivnet_conv_ba_relu_uint8_v0",
         "tuint8-b1-h10-r3-s1-c1-m1-wmin-1-wmax2-amin-1-amax1-imin-3-imax4-rqmin-30-rqmax30",
         "conv_ba_relu_uint8",
-        conv_ba_relu_uint8_compiler_options,
+        "--executors host 0 2 wave 1 --scheduler wave2  "
+        "--schedule_options ' --nname=generic --save_layer_output ' " # me options
+        "--partition from_multi 'conv_ba_relu_uint8/quantized_conv2d' 'conv_ba_relu_uint8/output' ",
         "--diff_options '--tolerance 1 1'"], # rt options
     "0-1conv1ba1relu_b1h1r1s1c1m1_uint8_wave": [
         "trivnet_conv_ba_relu_uint8_v0",
         "tuint8-b1-h1-r1-s1-c1-m1-wmin-1-wmax1-amin-1-amax1-imin-1-imax1-rqmin-30-rqmax30-hasba1-hasrelu1",
         "conv_ba_relu_uint8",
-        conv_ba_relu_uint8_compiler_options,
+        "--executors host 0 2 wave 1 --scheduler wave2  "
+        "--schedule_options ' --nname=generic --save_layer_output ' " # me options
+        "--partition from_multi 'conv_ba_relu_uint8/quantized_conv2d' 'conv_ba_relu_uint8/output' ",
         ""], # rt options
     "1-1conv1ba1relu_uint8_wave": [
         "trivnet_conv_ba_relu_uint8_v0",
         "tuint8-b8-h10-r3-s1-c3-m4-wmin-1-wmax2-amin-1-amax1-imin-1-imax1-rqmin-30-rqmax30-hasba1-hasrelu1",
         "conv_ba_relu_uint8",
-        conv_ba_relu_uint8_compiler_options,
+        "--executors host 0 2 wave 1 --scheduler wave2  "
+        "--schedule_options ' --nname=generic --save_layer_output ' " # me options
+        "--partition from_multi 'conv_ba_relu_uint8/quantized_conv2d' 'conv_ba_relu_uint8/output' ",
         ""], # rt options
     "1-1conv1relu_uint8_wave": [
         "trivnet_conv_ba_relu_uint8_v0",
         "tuint8-b8-h10-r3-s1-c3-m4-wmin-1-wmax1-amin-1-amax1-imin-1-imax1-rqmin-30-rqmax30-hasrelu1",
         "conv_ba_relu_uint8",
-        conv_ba_relu_uint8_compiler_options,
+        "--executors host 0 2 wave 1 --scheduler wave2  "
+        "--schedule_options ' --nname=generic --save_layer_output ' " # me options
+        "--partition from_multi 'conv_ba_relu_uint8/quantized_conv2d' 'conv_ba_relu_uint8/output' ",
         ""], # rt options
     "1-1conv1ba_uint8_wave": [
         "trivnet_conv_ba_relu_uint8_v0",
         "tuint8-b8-h10-r3-s1-c3-m4-wmin-1-wmax1-amin-1-amax1-imin-1-imax1-rqmin-30-rqmax30-hasba1",
         "conv_ba_relu_uint8",
-        conv_ba_relu_uint8_compiler_options,
+        "--executors host 0 2 wave 1 --scheduler wave2  "
+        "--schedule_options ' --nname=generic --save_layer_output ' " # me options
+        "--partition from_multi 'conv_ba_relu_uint8/quantized_conv2d' 'conv_ba_relu_uint8/output' ",
         ""], # rt options
 }
 
