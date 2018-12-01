088084dccc1c0a58757b24b4b5c84bb15d08d3ea
Jeffrey Huynh <jthuynh@amazon.com>
2018-12-01 18:25:02 +0000

    kaena-1047: add missing file
    Change-Id: Ied4191298888311f006277bd89ef824359a26b8d
    Signed-off-by: Jeffrey Huynh <jthuynh@amazon.com>

---
+++ test/e2e/trivnet_add_2in.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2017, Amazon.com. All Rights Reserved
+#
+# Unit test for Tffe - biasadd followed by residual add
+# The weight ranges are used for the bias add
+
+from trivnet_common import *
+
+IF1 = np.zeros([conf.B, conf.H, conf.H, conf.C])
+BIAS1 = np.zeros([conf.C])
+
+wval   = conf.gen_array_linspace(conf.WMIN, conf.WMAX, BIAS1.shape)
+w      = conf.gen_variable_tensor(name = conf.netName + "/bias1", initializer = wval)
+
+i0 = tf.placeholder(conf.tfDataType, shape=IF1.shape, name="input0")
+i1 = tf.placeholder(conf.tfDataType, shape=IF1.shape, name="input1")
+if conf.joinType == "SUB":
+    i2 = i1 - i0
+else:    
+    i2 = tf.add(i1, i0, name=conf.netName + "/i2")
+
+output = tf.identity(i2, name=conf.netName+"/output")
+
+i0val  = conf.gen_array_rand(conf.IMIN, conf.IMAX, IF1.shape)
+i1val  = conf.gen_array_rand(conf.IMIN, conf.IMAX, IF1.shape)
+
+conf.gen_graph(output, input_data={'input0:0' : i0val, 'input1:0' : i1val})
